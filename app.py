050d95f1a17e1cd62683e4ce60c60c742c3afed2
Jacob Daley <larksongdaley@gmail.com>
2024-07-04 22:29:38 -0700

    Created __init__.py

--- app.py
+++ app.py
@@ -1,13 +1,3 @@
-# config    
-from flask import Flask
-app = Flask(__name__)  # This line creates a Flask application instance.
+from petfax import create_app
 
-# index route
-@app.route('/')        # This decorator specifies that the function below handles requests to the root URL ('/').
-def index():           # This defines a function named 'index' which will be called when the root URL is accessed.
-    return 'Hello, this is PetFax!'  # The function returns a string when the root URL is accessed.
-
-# pets index route
-@app.route('/pets')    # This decorator specifies that the function below handles requests to the '/pets' URL.
-def pets():            # This defines a function named 'pets' which will be called when the '/pets' URL is accessed.
-    return 'These are our pets available for adoption!'  # The function returns a string when the '/pets' URL is accessed.
+app = create_app()
