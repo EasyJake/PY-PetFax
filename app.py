698fb4f823c9f1501f6eeca7801b5b3b16e9c664
Jacob Daley <larksongdaley@gmail.com>
2024-07-07 21:51:03 -0700

    Completed Activity (ppp-5-flask-routes-and-the-application-factory)

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ app = create_app()
 if __name__ == "__main__":
     app.run(debug=True)  # Turn off debug mode in production
 
-
-
-
-
-# from petfax import create_app
-# app = create_app()
+# New route
+@app.route('/pets')
+def pets():
+    return 'These are our pets available for adoption!'
